6165ef67b0bfdd2e6577e01f1815396f4ca178dd
holaalex2204 <holaalex2204@hotmail.com>
2013-05-19 17:51:54 -0500

    correcciones FuncionesDB

--- dao/funcionesBD.php
+++ dao/funcionesBD.php
@@ -6,7 +6,7 @@
 	function insertaUsuario($nombre,  $apellidos, $tel, $direccion, $mail, $nickname, $pass)
 	{
 		try {
-			$conn = new PDO('mysql:host=localhost;dbname=test', $user, $pass);
+			$conn = new PDO('mysql:host='.$url.';dbname='.$db, $user, $pass);
 			$qry = "INSERT INTO ".$db.".cliente  (correo, nombre, app, pass, telefono, direccion, nickname) VALUE ('".$mail."','".$nombre."','".$apellidos."','".$pass."','".$tel."','".$direccion."','".$nickname."');";
 			$cont = $conn->exec($qry);
 			$conn = null;
@@ -19,7 +19,7 @@
 	function existeUsuario($nickname, $pass)
 	{
 		try {
-			$conn = new PDO('mysql:host=localhost;dbname=test', $user, $pass);
+			$conn = new PDO('mysql:host='.$url.';dbname='.$db, $user, $pass);
 			$qry = "SELECT id from ".$db.".cliente  where   ".$db.".cliente.nickname LIKE CONCAT('%',".$nickname.",'%') and   ".$db.".cliente.pass  LIKE CONCAT('%',".$pass.",'%');";
 			foreach ($conn->query($qry) as $row) 
 			{
@@ -31,4 +31,18 @@
 		    die();
 		}
 	}
+	function insertaModelo($modelo, $year,$capPer,$ren,$cat,$foto,$precio)
+	{
+		try {
+			$conn = new PDO('mysql:host='.$url.';dbname='.$db, $user, $pass);
+			$qry = "INSERT INTO ".$db.".cliente  (nombre, year, capacidadPersonas, rendimiento, categoria, foto, precioDia) VALUE ('".$modelo."',".$year.",".$capPer.",".$ren.",'".$cat."','".$foto."');";
+			$cont = $conn->exec($qry);
+			$conn = null;
+		} catch (PDOException $e) {
+		    print "Error!: " . $e->getMessage() . "<br/>";
+		    die();
+		}
+		return $cont;
+	}
+	function insertaCarro($noserie, $idmodelo, $transmision){}
 ?>
