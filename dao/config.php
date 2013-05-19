d9ddc74af63b7372c0c56f59b8cc9126d5b25129
holaalex2204 <holaalex2204@hotmail.com>
2013-05-19 16:42:41 -0500

    funcionesCorregidas BD

--- dao/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$url = "localhost";
-	$usuario = "root";
-	$password = "root";
-	$db = "rentalcar";
-	include("php/DataConnection.class.php");
-	$db = new DataConnection($url, $usuario,$password);	
-?>
--- dao/funcionesBD.php
+++ dao/funcionesBD.php
@@ -1,10 +1,34 @@
 <?php
-	include("dao/DataConnection.class.php");
-	include("dao/config.php");
+	$url = "localhost";
+	$usuario = "root";
+	$password = "root";
+	$db = "rentalcar";
 	function insertaUsuario($nombre,  $apellidos, $tel, $direccion, $mail, $nickname, $pass)
 	{
-		$conn = new DataConnection($url, $usuario, $password);
-		$qry = "INSERT INTO ".$db.".cliente  (correo, nombre, app, pass, telefono, direccion, nickname) VALUE ('".$mail."','".$nombre."','".$apellidos."','".$pass."','".$tel."','".$direccion."','".$nickname."',);";
-		return $result = $conn->getDB($qry);
+		try {
+			$conn = new PDO('mysql:host=localhost;dbname=test', $user, $pass);
+			$qry = "INSERT INTO ".$db.".cliente  (correo, nombre, app, pass, telefono, direccion, nickname) VALUE ('".$mail."','".$nombre."','".$apellidos."','".$pass."','".$tel."','".$direccion."','".$nickname."');";
+			$cont = $conn->exec($qry);
+			$conn = null;
+		} catch (PDOException $e) {
+		    print "Error!: " . $e->getMessage() . "<br/>";
+		    die();
+		}
+		return $cont;
 	}	
+	function existeUsuario($nickname, $pass)
+	{
+		try {
+			$conn = new PDO('mysql:host=localhost;dbname=test', $user, $pass);
+			$qry = "SELECT id from ".$db.".cliente  where   ".$db.".cliente.nickname LIKE CONCAT('%',".$nickname.",'%') and   ".$db.".cliente.pass  LIKE CONCAT('%',".$pass.",'%');";
+			foreach ($conn->query($qry) as $row) 
+			{
+        				return $row['id'];
+			}
+			$conn = null;
+		} catch (PDOException $e) {
+		    print "Error!: " . $e->getMessage() . "<br/>";
+		    die();
+		}
+	}
 ?>
