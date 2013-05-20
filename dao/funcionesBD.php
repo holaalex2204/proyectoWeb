3503d8d895bc868cf170987c56060e05b26abefe
holaalex2204 <holaalex2204@hotmail.com>
2013-05-20 14:54:12 -0500

    correcciones Funciones =)

--- dao/funcionesBD.php
+++ dao/funcionesBD.php
@@ -9,7 +9,7 @@
 	{
 		global $url, $usuario, $password, $db;
 		try {
-			$conn = new PDO('mysql:host='.$url.';dbname='.$db, $usuario, $password);
+			$conn = new PDO('mysql:host='.$url.';dbname='.$db.';charset=utf8', $usuario, $password);
 			$conn>exec("SET CHARACTER SET utf8");
 			$qry = "INSERT INTO ".$db.".cliente  (correo, nombre, app, pass, telefono, direccion, nickname) VALUE ('".$mail."','".$nombre."','".$apellidos."','".$pass."','".$tel."','".$direccion."','".$nickname."');";
 			$cont = $conn->exec($qry);
@@ -24,7 +24,7 @@
 	{
 		global $url, $usuario, $password, $db;
 		try {
-			$conn = new PDO('mysql:host='.$url.';dbname='.$db, $usuario, $password);
+			$conn = new PDO('mysql:host='.$url.';dbname='.$db.';charset=utf8', $usuario, $password);
 			$conn>exec("SET CHARACTER SET utf8");
 			$qry = "SELECT ".$db.".cliente.id from ".$db.".cliente  where   ".$db.".cliente.nickname LIKE CONCAT('%','".$nickname."','%') and   ".$db.".cliente.pass  LIKE CONCAT('%','".$pass."','%');";
 			foreach ($conn->query($qry) as $row) 
@@ -41,7 +41,7 @@
 	{
 		global $url, $usuario, $password, $db;
 		try {
-			$conn = new PDO('mysql:host='.$url.';dbname='.$db, $usuario, $password);
+			$conn = new PDO('mysql:host='.$url.';dbname='.$db.';charset=utf8', $usuario, $password);
 			$conn>exec("SET CHARACTER SET utf8");
 			$qry = "INSERT IGNORE INTO ".$db.".modelo  (nombre, year, capacidadPersonas, rendimiento, categoria, foto, precioDia) VALUE ('".$modelo."',".$year.",".$capPer.",".$ren.",'".$cat."','".$foto."',".$precioDia.");";
 			$cont = $conn->exec($qry);
@@ -56,7 +56,7 @@
 	{
 		global $url, $usuario, $password, $db;
 		try {
-			$conn = new PDO('mysql:host='.$url.';dbname='.$db, $usuario, $password);
+			$conn = new PDO('mysql:host='.$url.';dbname='.$db.';charset=utf8', $usuario, $password);
 			$conn>exec("SET CHARACTER SET utf8");
 			$qry = "INSERT IGNORE INTO ".$db.".carro  (noSerie, modelo, status, transmision, sucursal) VALUE ('".$noserie."',".$idmodelo.",'Disponible','".$transmision."',".$sucursal.");";
 			$cont = $conn->exec($qry);
@@ -71,7 +71,7 @@
 	{
 		global $url, $usuario, $password, $db;
 		try {
-			$conn = new PDO('mysql:host='.$url.';dbname='.$db, $usuario, $password);
+			$conn = new PDO('mysql:host='.$url.';dbname='.$db.';charset=utf8', $usuario, $password);
 			$conn>exec("SET CHARACTER SET utf8");
 			$qry = "call insertaLugar('".$pais."','".$ciudad."','".$sitio."');";
 			$cont = $conn->query($qry);
@@ -86,10 +86,8 @@
 	{
 		global $url, $usuario, $password, $db;
 		try {
-			$conn = new PDO('mysql:host='.$url.';dbname='.$db, $usuario, $password);
-			$conn>exec("SET CHARACTER SET utf8");
+			$conn = new PDO('mysql:host='.$url.';dbname='.$db.';charset=utf8', $usuario, $password);
 			$qry = "INSERT INTO sucursal (nombre, tel, direccion, correo, pass)  VALUE ('".$nombre."','".$tel."','".$direccion."','".$correo."','".$pass."');";
-			echo($qry);
 			$cont = $conn->exec($qry);
 			$conn = null;
 		} catch (PDOException $e) {
@@ -98,7 +96,8 @@
 		}
 		return $cont;
 	}
-le 	//insertaCarro("D3783645",3,"Manual",1);
+	insertaSucursal("Aragón","57-10-7009","Valle de Tormes 174 Col. Valle de Aragón 3ra Sección","holaalex2204@hotmail.com","superdupi");
+ 	//insertaCarro("D3783645",3,"Manual",1);
 	//insertaModelo("Pontiac Matiz",2005,4,18.5,"Compacto","matiz2005.jpg",420);
 	//insertaSitio("México","Distrito Federal", "Central de Autobuses del Sur");
 	//existeUsuario('alex','superdupi');
